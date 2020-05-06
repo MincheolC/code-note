3cca3786539467863ffce83323ceb8c5e3068f75
MincheolC <mccha0407@gmail.com>
2020-05-06 18:37:02 +0900

    feat:js:react test setState

--- js/react-tutorial/src/App.js
+++ js/react-tutorial/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Game from './components/organisms/Game';
+import Home from './components/Home';
+import SetStateEx from './components/SetStateEx';
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      {/* <Game /> */}
+      {/* <Home /> */}
+      <SetStateEx />
     </div>
   );
 }
