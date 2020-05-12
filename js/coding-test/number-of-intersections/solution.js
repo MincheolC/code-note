062c4c1e4e79190c3ce85668d5ac2a756434f4b2
MincheolC <mccha0407@gmail.com>
2020-05-12 15:42:48 +0900

    feat: codility test 16 (IDK)

---
+++ js/coding-test/number-of-intersections/solution.js
@@ -0,0 +1,33 @@
+function getIntersectNum(leftPoints, rightPoint) {
+  let count = 0;
+  for (let i = 0; i < leftPoints.length; i += 1) {
+    if (leftPoints[i] > rightPoint) {
+      return count;
+    }
+    count += 1;
+  }
+  return count;
+}
+
+function solution(A) {
+  // write your code in JavaScript (Node.js 8.9.4)
+  const leftPoints = [];
+  const rightPoints = [];
+  const len = A.length;
+
+  for (let i = 0; i < len; i += 1) {
+    leftPoints.push(i - A[i]);
+    rightPoints.push(i + A[i]);
+  }
+
+  leftPoints.sort();
+  rightPoints.sort();
+
+  let intersections = 0;
+
+  for (let i = 0; i < len; i += 1) {
+    const num = getIntersectNum(leftPoints, rightPoints[i]);
+    intersections += num - 1 - i;
+  }
+  return intersections;
+}
