33034b78d2c1e224cc28c4d50c30f2db91a34d36
MincheolC <mccha0407@gmail.com>
2020-08-18 10:10:32 +0900

    feat: useMemo and useEffect

--- js/react-tutorial/src/components/atoms/User/index.js
+++ js/react-tutorial/src/components/atoms/User/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const User = ({ user, onRemove, onToggle }) => (
-  <div>
-    <b
-      style={{
-        cursor: 'pointer',
-        color: user.active ? 'green' : 'black'
-      }}
-      onClick={() => onToggle(user.id)}
-    >{user.username}</b>
-    <span>({user.email})</span>
-    <button onClick={() => onRemove(user.id)}>삭제</button>
-  </div>
-);
+const User = ({ user, onRemove, onToggle }) => {
+
+  useEffect(() => {
+    console.log(user);
+    return () => {
+      console.log(user);
+    }
+  }, [user]);
+
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >{user.username}</b>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  )
+};
 
 export default User;
--- js/react-tutorial/src/components/organisms/UserPage/index.js
+++ js/react-tutorial/src/components/organisms/UserPage/index.js
@@ -1,6 +1,11 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useMemo} from 'react';
 import UserList from '../../moleculars/UserList';
 import CreateUser from '../../atoms/CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중....');
+  return users.filter(user => user.active).length;
+}
 
 function UserPage() {
   const [inputs, setInputs] = useState({
@@ -60,11 +65,13 @@ function UserPage() {
   }
 
   const nextId = useRef(4);
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자 수: {count}</div>
     </>
   )
 }
