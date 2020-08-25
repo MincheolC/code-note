a27efe1170094a975aa887e7d395608c2d1bd046
MincheolC <mccha0407@gmail.com>
2020-08-25 14:47:52 +0900

    feat: react-router-dom (queryString, history, withRouter)

--- js/react-dependency-test/test-react-router-dom/src/App.js
+++ js/react-dependency-test/test-react-router-dom/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
-import Profile from "./components/Profile";
+import Profiles from "./components/Profiles";
+import HistorySample from "./components/HistorySample";
 
 function App() {
   return (
@@ -15,13 +16,17 @@ function App() {
           <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/profile/mincheol">Me</Link>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/history">히스토리</Link>
         </li>
       </ul>
       <hr />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
--- js/react-dependency-test/test-react-router-dom/src/components/About/index.js
+++ js/react-dependency-test/test-react-router-dom/src/components/About/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import qs from "qs";
 
-function About() {
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.detail === "true";
   return (
     <div>
       <h1>소개</h1>
       <p>Prject</p>
+      {detail && <p>Detail</p>}
     </div>
   );
 }
