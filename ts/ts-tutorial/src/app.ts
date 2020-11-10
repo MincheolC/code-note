0b9387856cfa58e7d1886cc8347781109af7f8ad
MincheolC <mccha0407@gmail.com>
2020-11-10 23:17:53 +0900

    feat: BasicTypes

--- ts/ts-tutorial/src/app.ts
+++ ts/ts-tutorial/src/app.ts
@@ -1,1 +1,1 @@
-console.log("hello typescript");
+require('./basicTypes');
---
+++ ts/ts-tutorial/src/basicTypes.ts
@@ -0,0 +1,45 @@
+/*
+ * Value로부터 쉽게 타입 추론이 되면 타입 선언을 하지 않아도 됨.
+ * (bigint, boolean, number, null, RegExp, string, symbol, undefined)
+ */
+// Boolean
+const isDone = false;
+console.log('[boolean]', isDone);
+// Number & bigInt
+const decimal = 10;
+const hex = 0xf00d;
+const binary = 0b1010;
+const octal = 0o744;
+const big = 100n;
+console.log('[number]', decimal, hex, binary, octal, big);
+// String
+const color = 'blue';
+const sentence = `i like ${color}`;
+console.log('[string]', sentence);
+// Array
+const list: number[] = [1, 2, 3];
+console.log('[array]', list);
+// Tuple
+const tuple: [string, number] = ['hello', 10];
+console.log('[tuple]', tuple, tuple[0], tuple[1]);
+// Enum
+enum Color {
+  Red = 1,
+  Green,
+  Blue,
+}
+const c: Color = Color.Green;
+const colorName: string = Color[2];
+console.log('[enum]', c, colorName);
+// Unknown
+const maybe: unknown = 'hello';
+if (maybe === true) {
+  const aBoolean: boolean = maybe;
+  console.log('[unknown]', aBoolean);
+} else if (typeof maybe === 'string') {
+  const aString: string = maybe;
+  console.log('[unknown]', aString);
+}
+// Any
+const looselyTyped: any = {};
+const d = looselyTyped.a.b.c.d;
