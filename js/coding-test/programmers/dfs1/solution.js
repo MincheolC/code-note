b6376cdbfaeaceb4c0bd43ac2daa0922dc5f840a
MincheolC <mccha0407@gmail.com>
2020-11-25 03:09:51 +0900

    feat: programmers dfs1

--- js/coding-test/programmers/dfs1/solution.js
+++ js/coding-test/programmers/dfs1/solution.js
@@ -1,9 +1,25 @@
-function solution(numbers, target) {
-  return dfs(numbers, target, 0, 0);
+// solution 1
+function dfs(numbers, target, sum) {
+  if (!numbers || numbers.length < 1) return sum === target ? 1 : 0;
+  const next = numbers.slice(1);
+  return dfs(next, target, sum + numbers[0]) + dfs(next, target, sum - numbers[0]);
 }
 
-function dfs(numbers, target, index, num) {
-  if (index === numbers.length) return num === target ? 1 : 0;
-  return dfs(numbers, target, index + 1, num + numbers[index]) + dfs(numbers, target, index + 1, num - numbers[index]);
+function solution(numbers, target) {
+  return dfs(numbers, target, 0);
 }
+
+// solution 2
+function dfs2(numbers, target, index, num) {
+  if (index === numbers.length) return num === target ? 1 : 0;
+  return (
+    dfs2(numbers, target, index + 1, num + numbers[index]) +
+    dfs2(numbers, target, index + 1, num - numbers[index])
+  );
+}
+
+function solution2(numbers, target) {
+  return dfs2(numbers, target, 0, 0);
+}
+
 console.log(solution([1, 1, 1, 1, 1], 3));
