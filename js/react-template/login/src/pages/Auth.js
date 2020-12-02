ddbfb2598e161c64e48c0edcb95e7bffbd89c507
MincheolC <mccha0407@gmail.com>
2020-12-02 22:42:02 +0900

    feat: react login page (form)

--- js/react-template/login/src/pages/Auth.js
+++ js/react-template/login/src/pages/Auth.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
+import { AuthWrapper } from '../components/Auth';
 import { useDispatch } from 'react-redux';
 import { setHeaderVisibility } from '../redux/modules/base';
+import { Route } from 'react-router-dom';
+import { Login, SignUp } from '../containers/Auth';
 
 function Auth() {
   const dispatch = useDispatch();
@@ -9,7 +12,12 @@ function Auth() {
     return () => dispatch(setHeaderVisibility(true));
   }, [dispatch])
 
-  return <div>Auth</div>;
+  return (
+    <AuthWrapper>
+      <Route path='/auth/login' component={Login} />
+      <Route path='/auth/signup' component={SignUp} />
+    </AuthWrapper>
+  );
 }
 
 export default Auth;
