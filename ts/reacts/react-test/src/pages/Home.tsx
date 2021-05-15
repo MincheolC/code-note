4f3bb151f023f93519ce8d58ca7537223c4aef9a
MincheolC <mccha0407@gmail.com>
2021-05-15 13:35:32 +0900

    feat:ts: useMemo Test

--- ts/reacts/react-test/src/pages/Home.tsx
+++ ts/reacts/react-test/src/pages/Home.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import TodoList from '../components/TodoList'
 import CInputForm from '../components/CInputForm'
 import { Todo } from '../shared/types'
 
+function sortAscending(data: number[]) {
+  const cData = [...data]
+  cData.sort((a, b) => a - b);
+  return cData;
+}
+
 const Home: React.FC = () => {
+  const [data, setData] = useState<number[]>([]);
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetch = () => {
+      const result = [5,6,7,1,2,4,9,10];
+      setData([...result]);
+    }
+    fetch();
+  }, []);
+
+  const mData = useMemo(() => sortAscending(data), [data]);
+
   const onRemove = (id: number) => setTodos(todos.filter(todo => todo.id !== id));
   const onSubmit = (input: string) => setTodos([...todos, { id: todos.length, todo: input }]);
 
   return (
     <>
+      <div>{`Sorted Data Memoization: ${mData.join(' ')}`}</div>
       <CInputForm onSubmit={onSubmit} />
       <TodoList todos={todos} onRemove={onRemove}/>
     </>
