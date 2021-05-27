374c0794f79e9cc93b487ec516b963c649ac41f7
MincheolC <mccha0407@gmail.com>
2021-05-27 13:31:39 +0900

    fix:js: chat-server typo

--- js/chatting/chat-server/users.js
+++ js/chatting/chat-server/users.js
@@ -19,7 +19,7 @@ function getUser(id) {
 }
 
 function deleteUser(id) {
-  const index = user.findIndex(user => user.id === id);
+  const index = users.findIndex(user => user.id === id);
   if (index !== -1) return users.splice(index, 1)[0];
 }
 
