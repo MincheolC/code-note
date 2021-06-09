a956f4ed08c77ba25f4fe10f611ab97dc5699f14
MincheolC <mccha0407@gmail.com>
2021-06-09 21:38:12 +0900

    feat:clojure: "hello world" created by clj-new

---
+++ clojure/clojure-tutorial/test/charles/clojure_tutorial_test.clj
@@ -0,0 +1,7 @@
+(ns charles.clojure-tutorial-test
+  (:require [clojure.test :refer :all]
+            [charles.clojure-tutorial :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
