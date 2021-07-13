5bb9a7bc8b0acd7963b2a7731f2a812a0ac91a12
MincheolCha <mc.cha@greenlabs.co.kr>
2021-07-13 15:18:14 +0900

    feat:clj: graphql (mutations)

--- clojure/web-server-tutorial/src/graphql/resolver/user.clj
+++ clojure/web-server-tutorial/src/graphql/resolver/user.clj
@@ -1,5 +1,5 @@
 (ns graphql.resolver.user
-  (:require [model :refer [fetch-users fetch-user]]))
+  (:require [model :as m]))
 
 (defn health-check
   [_ _ _]
@@ -7,13 +7,35 @@
 
 (defn get-users
   [{db :db} _ _]
-  (fetch-users db))
+  (m/fetch-users db))
 
 (defn get-user-by-id
   [{db :db} {id :id} _]
-  (fetch-user db id))
+  (m/fetch-user db id))
+
+(defn add-user
+  [{db :db} {name :name
+             age  :age} _]
+  (let [id (m/create-user db {:name name :age age})]
+    (m/fetch-user db id)))
+
+(defn update-user
+  [{db :db} {id   :id
+             name :name
+             age  :age} _]
+  (m/update-user db {:id id :name name :age age})
+  (m/fetch-user db id))
+
+(defn delete-user
+  [{db :db} {id :id} _]
+  (if-let [_ (pos? (m/delete-user db id))]
+    id
+    -1))
 
 (def resolver-map
-  {:health-check health-check
-   :get-users get-users
-   :get-user-by-id get-user-by-id})
+  {:health-check   health-check
+   :get-users      get-users
+   :get-user-by-id get-user-by-id
+   :add-user       add-user
+   :update-user    update-user
+   :delete-user    delete-user})
--- clojure/web-server-tutorial/src/model.clj
+++ clojure/web-server-tutorial/src/model.clj
@@ -11,6 +11,18 @@
                                           :where  [:= :id id]}))
 
 (defn create-user [db {:keys [name age]}]
-  (execute! db {:insert-into [:user]
-                :columns     [:name :age]
-                :values      [[name age]]} {:return-keys true}))
+  (let [result (execute! db {:insert-into [:user]
+                             :columns     [:name :age]
+                             :values      [[name age]]} {:return-keys true})]
+    (-> result first :GENERATED_KEY)))
+
+(defn update-user [db {:keys [id name age]}]
+  (execute! db {:update [:user]
+                :set    {:name name
+                         :age  age}
+                :where  [:= :id id]} {:return-keys true}))
+
+(defn delete-user [db id]
+  (let [result (execute! db {:delete-from [:user]
+                             :where       [:= :id id]})]
+    (-> result first :next.jdbc/update-count)))
