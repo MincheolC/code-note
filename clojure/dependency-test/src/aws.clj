730d34e194d4a162f7949d2c06b4fbfa075a79b3
MincheolCha <mc.cha@greenlabs.co.kr>
2021-07-15 09:48:44 +0900

    feat:clj: s3 delete multi objects

--- clojure/dependency-test/src/aws.clj
+++ clojure/dependency-test/src/aws.clj
@@ -26,12 +26,17 @@
                                    :Key    path
                                    :Body   (io/input-stream image)}}))
 
-(defn delete-images-from-s3
+(defn delete-image-from-s3
   [s3-client bucket path]
   (aws/invoke s3-client {:op      :DeleteObject
                          :request {:Key path
                                    :Bucket bucket}}))
 
+(defn delete-images-from-s3
+  [s3-client bucket path]
+  (aws/invoke s3-client {:op      :DeleteObjects
+                         :request {:Delete {:Objects [{:Key path}]}
+                                   :Bucket bucket}}))
 (defn upload []
   (let [s3-client (make-s3-client config)
         bucket-name (get-in config [:aws :community-s3])]
@@ -40,9 +45,23 @@
 (defn delete []
   (let [s3-client (make-s3-client config)
         bucket-name (get-in config [:aws :community-s3])]
+    (delete-image-from-s3 s3-client bucket-name path)))
+
+(defn deletes []
+  (let [s3-client (make-s3-client config)
+        bucket-name (get-in config [:aws :community-s3])]
     (delete-images-from-s3 s3-client bucket-name path)))
+
+(comment
+  ;; examaples
+  (def s3 (aws/client {:api :s3}))
+  (aws/ops s3)
+  (aws/doc s3 :PutObject)
+  (aws/doc s3 :DeleteObjects)
+  )
 
 (comment
   (upload)
   (delete)
+  (deletes)
   )
