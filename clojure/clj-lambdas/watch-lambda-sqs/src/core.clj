0b335394a953706cd5dcebc8e8ef3fb65e63a08c
MincheolCha <mc.cha@greenlabs.co.kr>
2021-07-29 21:52:46 +0900

    feat:clj: cloudwatch->lambda

---
+++ clojure/clj-lambdas/watch-lambda-sqs/src/core.clj
@@ -0,0 +1,59 @@
+(ns core
+  (:gen-class
+   :methods [^:static [handler [Object] Object]])
+  (:require [clojure.data.json :as json]
+            [cognitect.aws.client.api :as aws]
+            [clojure.walk :as w])
+  (:import
+   (java.util LinkedHashMap ArrayList)))
+
+(defn send-message [client]
+  (aws/invoke client {:op :SendMessage
+                      :request {:QueueUrl "https://sqs.ap-northeast-2.amazonaws.com/140131123595/test.fifo"
+                                :MessageDeduplicationId "20210710"
+                                :MessageGroupId "20210710"
+                                :MessageBody (json/write-str {:date-str "20210710"
+                                                              :specie-code "061401"})}}))
+
+(defn simplify
+  "모든 중첩 구조의 맵을 standard hash map으로 바꾸고, 시퀀스를 벡터로 변환시킵니다."
+  [m]
+  (w/postwalk
+   (fn [node]
+     (cond
+       (instance? LinkedHashMap node) (simplify (into {} node))
+       (or (seq? node)
+           (instance? ArrayList node)) (simplify (vec node))
+       :else node))
+   m))
+
+(defn parse [msg]
+  (-> (into {} msg)
+      w/keywordize-keys))
+
+(defn processing []
+  (let [sqs-client (aws/client {:api :sqs
+                                :region :ap-northeast-2})
+        queues (aws/invoke sqs-client {:op :ListQueues})]
+    (prn queues)
+    (send-message sqs-client)))
+
+(defn -handler
+  "람다에서는 자바의 LinkedHashMap으로 넘어오기 때문에, 모든 **중첩된 자료구조**를 클로저의 map으로 바꿔주는 과정이 필요함"
+  [msg]
+  (prn msg (type msg))
+  (if-let [dlq (-> msg parse :dlq)]
+    (prn dlq)
+    (prn msg))
+  ;; (processing)
+  )
+
+(comment
+  (def sqs (aws/client {:api :sqs}))
+  (aws/ops sqs)
+
+  (def dlq {"dlq" ["020101" "020199" "030101" "030107" "030108" "030110"]})
+  (def cloud-watch-msg {"version" "0", "id" "d385be19-519c-dcfb-6fbe-ab043ece9ed1", "detail-type" "Scheduled Event", "source" "aws.events", "account" "140131123595", "time" "2021-07-29T11:42:15Z", "region" "ap-northeast-2", "resources" ["arn:aws:events:ap-northeast-2:140131123595:rule/every-5-mins"], "detail" {}})
+  (-handler dlq)
+  (-handler cloud-watch-msg)
+  )
