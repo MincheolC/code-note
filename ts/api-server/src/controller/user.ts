ab6a32dd3113618771679280d12c95e6ad755f82
MincheolCha <mc.cha@greenlabs.co.kr>
2021-12-03 20:47:47 +0900

    feat:ts: api-server

---
+++ ts/api-server/src/controller/user.ts
@@ -0,0 +1,5 @@
+import { Request, Response, NextFunction } from 'express';
+import { UserModel} from '../model/user'
+
+export const getUser = (req: Request, res: Response) => {}
+export const createUser = () => {}
--- ts/api-server/src/index.ts
+++ ts/api-server/src/index.ts
@@ -1,10 +1,3 @@
-import express from 'express'
+import app from "./app";
 
-const PORT = 8080
-const app = express()
-
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Welcome to Share Profits API')
-})
-
-app.listen(PORT, () => console.log(`Start to listen at ${PORT}`))
+app.listen(app.get("port"), () => console.log(`App is running at http://localhost:${app.get("port")}`))
