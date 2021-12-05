b9229fb1d91a2f6a71360bb91a2054987ca94c39
MincheolCha <mc.cha@greenlabs.co.kr>
2021-12-05 21:25:49 +0900

    feat:clj: migratus add seed

--- clojure/migration-test/resources/migrations/20211205121449-init.up.sql
+++ clojure/migration-test/resources/migrations/20211205121449-init.up.sql
@@ -14,9 +14,13 @@ CREATE TABLE `lottery_history` (
     `date` DATETIME NOT NULL COMMENT "복권 추첨 날짜",
     `winning_numbers` VARCHAR(100) NOT NULL COMMENT "1~28 일반볼 5개, 0~9 파워볼 1개",
     `total_reward` BIGINT NOT NULL COMMENT "총 상금액",
+    `is_test` TINYINT(1) DEFAULT 0,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+--;;
+INSERT INTO `users` (`name`, `email`, `phone`) VALUES ("차민철", "mccha0407@gmail.com", "01066470203");
+--;;
+INSERT INTO `lottery_history` (`date`, `winning_numbers`, `total_reward`, `is_test`) VALUES ("2021-12-05", "1,2,3,4,5,6", 123456, 1);
