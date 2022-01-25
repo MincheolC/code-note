a55f37c067b03aed8af82cbbfd802fdd567f9f06
MincheolCha <mc.cha@greenlabs.co.kr>
2022-01-25 13:48:38 +0900

    feat:clj 웹 서버 보일러플레이트 (update)

--- clojure/web-server-boilerplate/src/database.clj
+++ clojure/web-server-boilerplate/src/database.clj
@@ -7,13 +7,15 @@
   (sql/format query {:dialect :mysql}))
 
 (defn execute!
-  ([ds query] (execute! ds query {:builder-fn rs/as-unqualified-kebab-maps
-                                  :return-keys true}))
+  ([ds query] (execute! ds query {:builder-fn rs/as-unqualified-kebab-maps}))
   ([ds query opt]
    (jdbc/execute! ds (->qs query) opt)))
 
 (defn execute-one!
-  ([ds query] (execute-one! ds query {:builder-fn rs/as-unqualified-kebab-maps
-                                      :return-keys true}))
+  ([ds query] (execute-one! ds query {:builder-fn rs/as-unqualified-kebab-maps}))
   ([ds query opt]
    (jdbc/execute-one! ds (->qs query) opt)))
+
+(defn insert! [ds query]
+  (execute-one! ds query {:builder-fn rs/as-unqualified-kebab-maps
+                          :return-keys true}))
--- clojure/web-server-boilerplate/src/graphql/user.clj
+++ clojure/web-server-boilerplate/src/graphql/user.clj
@@ -1,6 +1,11 @@
 (ns graphql.user
-  (:require [database :refer [execute! execute-one!]]
+  (:require [database :refer [insert! execute! execute-one!]]
             [next.jdbc :as jdbc]))
+
+(defn get-user-by-id-query [id]
+  {:select [:*]
+   :from   [:user]
+   :where  [:= :id id]})
 
 (defn get-users [{ds :db} _ _]
   (let [query {:select [:*]
@@ -9,13 +14,24 @@
 
 (defn create-user [{ds :db} {:keys [name phone]} _]
   (let [insert-query {:insert-into :user
-                      :values [{:name name :phone phone}]}
-        select-query (fn [id] {:select [:*]
-                               :from   [:user]
-                               :where  [:= :id id]})]
+                      :values [{:name name :phone phone}]}]
     (jdbc/with-transaction [tx ds]
-      (when-let [id (:generated-key (execute-one! tx insert-query))]
-        (execute-one! tx (select-query id))))))
+      (when-let [id (:generated-key (insert! tx insert-query))]
+        (execute-one! tx (get-user-by-id-query id))))))
+
+(defn update-user [{ds :db} {id :id username :name phone :phone} _]
+  (if (or username phone)
+    (let [update-query (cond-> {:update :user
+                                :set {}
+                                :where  [:= :id id]}
+                               name (assoc-in [:set :name] username)
+                               phone (assoc-in [:set :phone] phone))]
+      (jdbc/with-transaction [tx ds]
+        (execute-one! tx update-query)
+        (execute-one! tx (get-user-by-id-query id))))
+    (execute-one! ds (get-user-by-id-query id))))
 
 (def resolver-map {:get-users get-users
-                   :create-user create-user})
+                   :create-user create-user
+                   :update-user update-user})
+
