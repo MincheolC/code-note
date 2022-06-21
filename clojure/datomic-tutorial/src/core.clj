063a9abb73b6e57749f97d5fb5a4f51545994769
MincheolCha <mc.cha@greenlabs.co.kr>
2022-06-21 23:10:28 +0900

    clojure: datomic client api tutorial

--- clojure/datomic-tutorial/src/core.clj
+++ clojure/datomic-tutorial/src/core.clj
@@ -9,9 +9,14 @@
 
 (def client (d/client cfg))
 
-(d/create-database client {:db-name "movies"})
-(def conn (d/connect client {:db-name "movies"}))
+;; Create a Database
+(d/create-database client {:db-name "dev"})
+;; Connect to database
+(def conn (d/connect client {:db-name "dev"}))
 
+;; :db/ident 속성은 엔티티의 고유한 이름을 지정
+;; :db/cardinality 1:1 or 1:n 관계 설정
+;; :db/doc 커멘트 같은 역할
 (def movie-schema [{:db/ident :movie/title
                     :db/valueType :db.type/string
                     :db/cardinality :db.cardinality/one
@@ -27,8 +32,6 @@
                     :db/cardinality :db.cardinality/one
                     :db/doc "The year the movie was released in theaters"}])
 
-(d/transact conn {:tx-data movie-schema})
-
 (def first-movies [{:movie/title "The Goonies"
                     :movie/genre "action/adventure"
                     :movie/release-year 1985}
@@ -39,14 +42,18 @@
                     :movie/genre "punk dystopia"
                     :movie/release-year 1984}])
 
-(d/transact conn {:tx-data first-movies})
+(defn insert! [connection data]
+  (d/transact connection {:tx-data data}))
 
-(def db (d/db conn))
-(def all-titles-q '[:find ?movie-title
-                    :where [_ :movie/title ?movie-title]])
+(defn fetch! [connection query]
+  (d/q query (d/db connection)))
 
-(d/q all-titles-q db)
+(comment
+  (def all-titles-q '[:find ?movie-title
+                      :where [_ :movie/title ?movie-title]])
+  (insert! conn movie-schema)
+  (insert! conn first-movies)
 
-(binding [*event-id* "test-event-id"]
-  (log/info "hello")
-  (test/ok))
+  (fetch! conn all-titles-q)
+
+  (d/delete-database client {:db-name "dev"}))
