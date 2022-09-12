e96e74c82e84af68ee3bc6d2e454b09f75adaf04
MincheolCha <mc.cha@greenlabs.co.kr>
2022-09-12 13:41:12 +0900

    feat:wedding-ecard init

---
+++ js/reacts/wedding-e-card/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="main-img.jpeg" className="" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
